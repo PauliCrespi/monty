81731a891aab02b8cdca9f4fc7e74735edc84d27
PauliCrespi <4377@holbertonschool.com>
2022-05-11 06:40:38 -0700

    .h

---
+++ main.h
@@ -0,0 +1,49 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+/**
+* struct stack_s - doubly linked list representation of a stack (or queue)
+* @n: integer
+* @prev: points to the previous element of the stack (or queue)
+* @next: points to the next element of the stack (or queue)
+*
+* Description: doubly linked list node structure
+* for stack, queues, LIFO, FIFO
+*/
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+        char *opcode;
+        void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/**funcs */
+int main(int argc, char *argv[]);
+void command_interpreter(char *argv);
+char **tokenizer(char *line, const char *sep);
+int op_comands(stack_t **stack, char **token, int count);
+
+#endif /* MAIN_H */
