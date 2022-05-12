25392b41daa2da53a59a0d14e7578a37c7ee86f3
PauliCrespi <4377@holbertonschool.com>
2022-05-12 08:20:51 -0700

    is digit

---
+++ isdigit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *_isdigit - checking for a digit
+ *@c : input
+ *Return: 1 if digit, 0 if else
+ */
+int _isdigit(char *c)
+{
+	char *string = c;
+
+	if (c == NULL)
+		return (0);
+	if (*string == '-')
+		string++;
+	for (; *string != '\0'; string++)
+	{
+		if ((*string < '0' || *string > '9'))
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
