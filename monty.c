8eaa0d3f5855f88e78fdf8f3d9b7814ded04afda
PauliCrespi <4377@holbertonschool.com>
2022-05-12 10:39:32 -0700

    monty

--- monty.c
+++ monty.c
@@ -26,8 +26,7 @@ void command_interpreter(char *argv)
 	char *token1 = NULL, *token2 = NULL, *line;
 	size_t len = 0;
 	stack_t *stack = NULL;
-	int l_num = 1;
-	int found = 0;
+	int l_num = 1, found = 0;
 
 	file = fopen(argv, "r");
 	if (file)
@@ -36,20 +35,26 @@ void command_interpreter(char *argv)
 		{
 			token1 = strtok(line, " \t\n");
 			if (token1 == NULL || token1[0] == '#')
-			{
 				continue;
-			}
 			token2 = strtok(NULL, " \t\n");
 			if (token2 != NULL && _isdigit(token2) == 1)
 				value = atoi(token2);
 			found = op_commands(&stack, token1, l_num);
 			if (found == 1)
-				dprintf(STDERR_FILENO, "L%d: usage: push integer\n", l_num);
+			{
+				free(line);
+				fclose(file);
+				push_error(stack, l_num);
+			}
 			else if (found == 2)
-				dprintf(STDERR_FILENO, "L%s: unknown instruction %d\n", token2, l_num);
+			{
+				free(line);
+				fclose(file);
+				ins_error(stack, l_num);
+			}
 		}
 		free(line);
-		free(stack);
+		free_list(stack);
 		fclose(file);
 	}
 	else
