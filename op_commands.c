7d4cd22326ddec570f1829d456449e8308d82e8b
PauliCrespi <4377@holbertonschool.com>
2022-05-12 09:22:23 -0700

    commands

--- op_commands.c
+++ op_commands.c
@@ -1,11 +1,13 @@
+#include "main.h"
+int value;
 /**
- * get_opc - function to handle the opcode
+ * op_commands - function to handle the opcode
  * @stack: is a stack or queue
- * @token : line
- * @count: is a line command
- * Return: nothing
+ * @token1 : line
+ * @line_number: int
+ * Return: int
  */
-int op_comands(stack_t **stack, char **token, int count)
+int op_commands(stack_t **stack, char *token1, unsigned int line_number)
 {
 	int i = 0;
 
@@ -16,28 +18,15 @@ int op_comands(stack_t **stack, char **token, int count)
 		{"pop", _pop},
 		{"swap", _swap},
 		{"add", _add},
-		{"sub", _sub},
 		{"nop", _nop},
-		{"div", _div},
-		{"mul", _mul},
-		{"mod", _mod},
-		{"pchar", _pchar},
-		{"pstr", _pstr},
 		{NULL, NULL}
 	};
 
 	while (op[i].opcode)
 	{
-		if (!strcmp(token[0], op[i].opcode))
+		if (!strcmp(token1, op[i].opcode))
 		{
-			if (!strcmp(token[0], "push"))
-			{
-				if (_isdigit(token[1]) == 1)
-					value = atoi(token[1]);
-				else
-					return (1);
-			}
-			op[i].f(stack, (unsigned int)count);
+			op[i].f(stack, (unsigned int) line_number);
 			break;
 		}
 		i++;
